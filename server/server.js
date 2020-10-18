2f7792ea2cd424042e4d5783017ca3686e6111cb
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2020-10-18 06:10:35 +0000

    changed http to https server in backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
-const express = require('express');
 
+const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/realtimerps.com/privkey.pem');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/realtimerps.com/fullchain.pem');
+const credentials = {key: privateKey, cert: certificate};
+
+const server = require('https').createServer(credentials, app);
 const io = require('socket.io')(server);
 
 const mongoose = require('mongoose');
